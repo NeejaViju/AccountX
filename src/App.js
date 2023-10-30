c3aac863f0ed9ec81ef8c88d2e5b6eeb30197d00
SARMATHIk <sarmathi1906@gmail.com>
2023-10-30 13:42:47 +0530

    bramch and registered address updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import './App.css';
-import 'font-awesome/css/font-awesome.min.css';
+import React, { useState } from "react";
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faSearch, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
@@ -22,7 +22,7 @@ function NavBar() {
   return (
     <div className="navbar">
       <img src="src\Capture.PNG" alt="Logo" className="navbar-logo" />
-      
+
       {/* Add Icon */}
       <div className="add-icon-container">
         <i className="fa fa-plus-circle" aria-hidden="true"></i>
@@ -32,17 +32,14 @@ function NavBar() {
         <i className="fa fa-search" aria-hidden="true"></i>
         <input type="text" placeholder="Search..." className="navbar-search" />
       </div>
-      
+
       <button className="btn voucher-entry">VOUCHER ENTRY</button>
       <button className="btn upgrade">UPGRADE</button>
-    
 
       <i className="fa fa-bell" aria-hidden="true"></i>
       <div className="profile-container">
-        
         <select className="profile-dropdown">
           <option>ABC Private Limited</option>
-      
         </select>
         <i className="fa fa-cog" aria-hidden="true"></i>
         <i className="fa fa-bars" aria-hidden="true"></i>
@@ -51,74 +48,69 @@ function NavBar() {
   );
 }
 
-
-
 // SideBar Component
 function SideBar() {
   const [showSubmenu, setShowSubmenu] = useState(false);
 
   return (
-      <div className="sidebar">
-          <h3>📊 Dashboard</h3>
+    <div className="sidebar">
+      <h3>📊 Dashboard</h3>
 
-          <div 
-              className="menu-item master"
-              onClick={() => setShowSubmenu(!showSubmenu)}
-          >
-              🛠 Masters
-          </div>
-
-          {showSubmenu && (
-              <div className="submenu">
-                  <div>Company Profile</div>
-                  <div>Item Master</div>
-                  <div>Ledger Master</div>
-                  <div>Account Master</div>
-                  <div>Tax Master</div>
-                  <div>Employee Master</div>
-                  <div>Opening Balance</div>
-
-
-                  {/* ... Add the other submenu items similarly ... */}
-              </div>
-          )}
-
-          <div className="menu-item">🛒 Sales</div>
-          <div className="menu-item">📦 Purchases</div>
-          <div className="menu-item">💼 Expenses</div>
-          <div className="menu-item">🏦 Banking</div>
-          <div className="menu-item">✅ Approvals</div>
-          <div className="menu-item">💹 Accounts</div>
-          {/* ... Repeat similar divs for the other sections ... */}
-          <div className="menu-item">📊 Reports</div>
-          <div className="menu-item">📃 e-Way Bills</div>
-          <div className="menu-item">📜 e-Invoices</div>
-          <div className="menu-item">📋 Task</div>
-          <div className="menu-item">🎉 Promotions</div>
-          <div className="menu-item">📁 Tax Filings</div>
-          <div className="menu-item">📄 Subscriptions</div>
+      <div
+        className="menu-item master"
+        onClick={() => setShowSubmenu(!showSubmenu)}
+      >
+        🛠 Masters
       </div>
+
+      {showSubmenu && (
+        <div className="submenu">
+          <div>Company Profile</div>
+          <div>Item Master</div>
+          <div>Ledger Master</div>
+          <div>Account Master</div>
+          <div>Tax Master</div>
+          <div>Employee Master</div>
+          <div>Opening Balance</div>
+
+          {/* ... Add the other submenu items similarly ... */}
+        </div>
+      )}
+
+      <div className="menu-item">🛒 Sales</div>
+      <div className="menu-item">📦 Purchases</div>
+      <div className="menu-item">💼 Expenses</div>
+      <div className="menu-item">🏦 Banking</div>
+      <div className="menu-item">✅ Approvals</div>
+      <div className="menu-item">💹 Accounts</div>
+      {/* ... Repeat similar divs for the other sections ... */}
+      <div className="menu-item">📊 Reports</div>
+      <div className="menu-item">📃 e-Way Bills</div>
+      <div className="menu-item">📜 e-Invoices</div>
+      <div className="menu-item">📋 Task</div>
+      <div className="menu-item">🎉 Promotions</div>
+      <div className="menu-item">📁 Tax Filings</div>
+      <div className="menu-item">📄 Subscriptions</div>
+    </div>
   );
 }
-
 
 // MainContent Component
 function MainContent() {
   return (
     <div className="main-content">
-      <CompanyProfileHeader/>
-      <CompanyProfile/>
-      {/* <Addresses />
-      <PersonalDetails />
+      <CompanyProfileHeader />
+      <CompanyProfile />
+      <RegisteredAddresses />
+      <BranchAddresses />
+      {/* <PersonalDetails />
       <BusinessLegalInfo />
       <BankAccountDetails />
       <SigningContactPerson />
-      <ActionButtons /> */}
+      <ActionButtons />  */}
     </div>
   );
 }
-
-
 
 function CompanyProfileHeader() {
   return (
@@ -126,9 +118,19 @@ function CompanyProfileHeader() {
       <div className="profile-title">Company Profile</div>
       <div className="company-type">
         <span>Company Type</span>
-        <input type="radio" id="registered" name="companyType" value="registered" />
+        <input
+          type="radio"
+          id="registered"
+          name="companyType"
+          value="registered"
+        />
         <label htmlFor="registered">Registered</label>
-        <input type="radio" id="unregistered" name="companyType" value="unregistered" />
+        <input
+          type="radio"
+          id="unregistered"
+          name="companyType"
+          value="unregistered"
+        />
         <label htmlFor="unregistered">Unregistered</label>
       </div>
       <button className="back-btn">← Back</button>
@@ -137,12 +139,10 @@ function CompanyProfileHeader() {
   );
 }
 
-
 function CompanyProfile() {
   return (
     <div className="cp-company-container">
       <div className="cp-column">
-
         {/* First Row */}
         <div className="cp-row">
           <input type="text" placeholder="GST Number" />
@@ -171,20 +171,146 @@ function CompanyProfile() {
             <i className="fa fa-search icon-button"></i>
           </div>
         </div>
-
       </div>
 
       <div className="cp-label-box">Upload your logo</div>
     </div>
   );
 }
+//   Registered Addresses Component Here
 
+function RegisteredAddresses() {
+  return (
+    <div className="registeredOfficeAddress">
+      <h2>Registered Office Address</h2>
 
+      <div className="cp-company-container">
+        <div className="cp-column">
+          {/* First Row */}
+          <div className="cp-row">
+            <div className="cp-input-with-dropdown">
+              <div className="profile-container">
+                <input type="text" placeholder="Address Type" />
+                <button className="dropdown-icon-right"></button>
+              </div>
+            </div>
+            {/* + add button */}
+            <div className="cp-input-with-dropdown">
+              <input type="text" placeholder="Select Branch" />
+              <button className="dropdown-icon-right"></button>
+            </div>
+            {/* + add button */}
+            <div className="cp-input-with-dropdown">
+              <input type="text" placeholder="Unit/Division" />
+              <button className="dropdown-icon-right"></button>
+            </div>
+          </div>
+          {/* + add button */}
+          {/* Second Row */}
+          <div className="cp-row">
+            <input type="text" placeholder="Door No" />
+            <input type="text" placeholder="Building Name" />
+            <input type="text" placeholder="Street Name" />
+            <input type="text" placeholder="Area/Taluk" />
+          </div>
+          {/* Third Row */}
+          <div className="cp-row">
+            <input type="text" placeholder="City" />
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="State" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="District" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <input type="text" placeholder="PIN Code" />
+            {/* </div> */}
+          </div>
+          {/* fourth row */}
+          <div className="cp-row">
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="Country" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <input type="text" placeholder="GST Number" />
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="Place of Supply" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+//Branch Addresses here
+function BranchAddresses() {
+  return (
+    <div className="branchAddresses">
+      <h2> Branch Office Address</h2>
 
-
-// function Addresses() {
-//   return <div>Addresses Component Here...</div>;
-// }
+      <div className="cp-company-container">
+        <div className="cp-column">
+          {/* First Row */}
+          <div className="cp-row">
+            <div className="cp-input-with-dropdown">
+              <div className="profile-container">
+                <input type="text" placeholder="Address Type" />
+                <button className="dropdown-icon-right"></button>
+              </div>
+            </div>
+            {/* + add button */}
+            <div className="cp-input-with-dropdown">
+              <input type="text" placeholder="Select Branch" />
+              <button className="dropdown-icon-right"></button>
+            </div>
+            <div className="cp-input-with-dropdown">
+              <input type="text" placeholder="Unit/Division" />
+              <button className="dropdown-icon-right"></button>
+            </div>
+          </div>
+          {/* + add button */}
+          {/* Second Row */}
+          <div className="cp-row">
+            <input type="text" placeholder="Door No" />
+            <input type="text" placeholder="Building Name" />
+            <input type="text" placeholder="Street Name" />
+            <input type="text" placeholder="Area/Taluk" />
+          </div>
+          {/* Third Row */}
+          <div className="cp-row">
+            <input type="text" placeholder="City" />
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="State" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="District" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <input type="text" placeholder="PIN Code" />
+            {/* </div> */}
+          </div>
+          {/* fourth row */}
+          <div className="cp-row">
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="Country" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            <input type="text" placeholder="GST Number" />
+            <div className="cp-input-with-icon">
+              <input type="text" placeholder="Place of Supply" />
+              <i className="fa fa-search icon-button"></i>
+            </div>
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 // function PersonalDetails() {
 //   return <div>PersonalDetails Component Here...</div>;
