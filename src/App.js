73b7f87186098fb4eb0001a6f2febed08b0f313d
NeejaViju <neejaviju22@gmail.com>
2023-11-07 15:40:33 +0530

    flotaing lable menu color fixed

--- src/App.js
+++ src/App.js
@@ -58,15 +58,15 @@ function NavBar() {
 
       <div className="new-profile-container">
         <i className="fa fa-bell new-bell-icon" aria-hidden="true"></i>
-      
-          <select class="drop-down-nav-bar">
-            <option value="" disabled="" hidden="">ABC Private Limited</option>
-            <option value="type1">Type 1</option>
-            <option value="type2">Type 2</option>
-            <option value="type3">Type 3</option>
-            </select>
-           
-        
+
+        <select class="drop-down-nav-bar">
+          <option value="" disabled="" hidden="">ABC Private Limited</option>
+          <option value="type1">Type 1</option>
+          <option value="type2">Type 2</option>
+          <option value="type3">Type 3</option>
+        </select>
+
+
         <i className="fa fa-cog new-cog-icon" aria-hidden="true"></i>
         <i className="fa fa-bars new-bars-icon" aria-hidden="true"></i>
       </div>
@@ -174,10 +174,42 @@ function CompanyProfileHeader() {
   );
 }
 
+/*Company Profile*/
+
 function CompanyProfile() {
+
+  /*Floating Label*/
+  const [inputs, setInputs] = useState({
+    gstNumber: '',
+    companyType: '',
+    companyId: '',
+    businessTradeName: '',
+    businessLegalName: '',
+    mobileNumber: '',
+    businessEmail: '',
+    industry: '',
+  });
+  const [focused, setFocused] = useState('');
   const [logo, setLogo] = useState(null);
   const fileInputRef = useRef(null);
 
+  const handleFocus = (fieldName) => {
+    setFocused(fieldName);
+  };
+
+  const handleBlur = (fieldName) => {
+    if (!inputs[fieldName]) {
+      setFocused('');
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
+  };
+
+
+  /*file upload*/
   const handleLogoUpload = (event) => {
     const file = event.target.files[0];
     if (
@@ -185,7 +217,6 @@ function CompanyProfile() {
       file.type.startsWith("image/") &&
       file.size <= 5 * 1024 * 1024
     ) {
-      // Checking if it's an image and is under 5MB
       const reader = new FileReader();
       reader.onloadend = () => {
         setLogo(reader.result);
@@ -201,24 +232,63 @@ function CompanyProfile() {
       <div className="cp-column">
         {/* First Row */}
         <div className="cp-row">
-          <input type="text" placeholder="GST Number*" className="address-border" />
-          <div className="cp-input-with-dropdown">
-            <select className="address-border">
-              <option value="" disabled selected hidden>
-                Company Type*
-              </option>
-              <option value="type1">Type 1</option>
+          {/* GST Number Input */}
+          <div className="floating-label-group">
+            <input
+              type="text"
+              name="gstNumber"
+              placeholder="33WGOPW1208B2Z9"
+              className={`address-border ${inputs.gstNumber ? 'filled' : ''}`}
+              onChange={handleInputChange}
+              onFocus={() => handleFocus('gstNumber')}
+              onBlur={() => handleBlur('gstNumber')}
+              value={inputs.gstNumber}
+            />
+            <label htmlFor="gstNumber" className={focused === 'gstNumber' || inputs.gstNumber ? 'floating' : ''}>
+              GST Number*
+            </label>
+          </div>
+
+          {/* Company Type Dropdown */}
+          <div className="cp-input-with-dropdown floating-label-group">
+            <select
+              name="companyType"
+              className={`address-border ${inputs.companyType ? 'filled' : ''}`}
+              onChange={handleInputChange}
+              onFocus={() => handleFocus('companyType')}
+              onBlur={() => handleBlur('companyType')}
+              value={inputs.companyType}
+            >
+              <option value="" disabled></option>
+              <option value="type1">Proprietorship</option>
               <option value="type2">Type 2</option>
               <option value="type3">Type 3</option>
             </select>
+            <label htmlFor="companyType" className={focused === 'companyType' || inputs.companyType ? 'floating' : ''}>
+              Company Type*
+            </label>
             <button className="dropdown-icon-right">
               <i className="fa fa-chevron-down"></i>
             </button>
           </div>
 
-          <input type="text" placeholder="Company ID" className="address-border" /> 
+          {/* Company ID Input */}
+          <div className="floating-label-group">
+            <input
+              type="text"
+              name="companyId"
+              placeholder="Company ID"
+              className={`address-border ${inputs.companyId ? 'filled' : ''}`}
+              onChange={handleInputChange}
+              onFocus={() => handleFocus('companyId')}
+              onBlur={() => handleBlur('companyId')}
+              value={inputs.companyId}
+            />
+            <label htmlFor="companyId" className={focused === 'companyId' || inputs.companyId ? 'floating' : ''}>
+              Company ID
+            </label>
+          </div>
         </div>
-
         {/* Second Row */}
         <div className="cp-row">
           <input type="text" placeholder="Business Trade Name*" className="address-border" />
@@ -227,18 +297,25 @@ function CompanyProfile() {
 
         {/* Third Row */}
         <div className="cp-row">
-          <div className="cp-input-with-icon mobile">
-            <span className="country-code">+91</span>
-            <input type="text" placeholder="Mobile Number*"  className="address-border"/>
-          </div>
-          <input type="email" placeholder="Business Email*" className="address-border"/>
+
+        <div class="cp-input-with-icon mobile">
+          <div class="inputs-container">
+            <div class="mobileNumer-dropdown">
+              <select class="mobileDropDown"><option value="miss">+91</option>
+              <option value="miss">+11</option>
+              
+              </select>
+              <span class="dropdown-icon">
+                </span></div></div>
+                <input type="text" placeholder="Mobile Number*" class=""/>
+                </div>
+                <span className="separator"></span> 
+          <input type="email" placeholder="Business Email*" className="address-border" />
           <div className="cp-input-with-icon">
-            <input type="text" placeholder="Industry" className="address-border"/>
-            <i
-              className="fa fa-search new-search-icon-inside-input"
-              aria-hidden="true"
-            ></i>
+            <input type="text" placeholder="Industry" className="address-border" />
+            <i className="fa fa-search new-search-icon-inside-input" aria-hidden="true"></i>
           </div>
+
         </div>
       </div>
 
@@ -300,47 +377,37 @@ function MenuBar() {
   return (
     <div>
       <div className="menu-bar-container">
-        <button
-          className={`menu-bar-button ${
-            activeComponent === "Addresses" ? "active" : ""
-          }`}
-          onClick={() => handleClick("Addresses")}
-        >
-          Addresses
-        </button>
-        <button
-          className={`menu-bar-button ${
-            activeComponent === "PersonalDetails" ? "active" : ""
-          }`}
-          onClick={() => handleClick("PersonalDetails")}
-        >
-          Personal Details
-        </button>
-        <button
-          className={`menu-bar-button ${
-            activeComponent === "BusinessLegalInformation" ? "active" : ""
-          } business-button`}
-          onClick={() => handleClick("BusinessLegalInformation")}
-        >
-          Business Legal Information
-        </button>
-        <button
-          className={`menu-bar-button ${
-            activeComponent === "BankDetails" ? "active" : ""
-          }`}
-          onClick={() => handleClick("BankDetails")}
-        >
-          Bank Details
-        </button>
-        <button
-          className={`menu-bar-button ${
-            activeComponent === "ContactPersons" ? "active" : ""
-          }`}
-          onClick={() => handleClick("ContactPersons")}
-        >
-          Contact Persons
-        </button>
-      </div>
+  <button
+    className={`menu-bar-button ${activeComponent === "Addresses" ? "active-addresses" : ""}`}
+    onClick={() => handleClick("Addresses")}
+  >
+    Addresses
+  </button>
+  <button
+    className={`menu-bar-button ${activeComponent === "PersonalDetails" ? "active-personal-details" : ""}`}
+    onClick={() => handleClick("PersonalDetails")}
+  >
+    Personal Details
+  </button>
+  <button
+    className={`menu-bar-button ${activeComponent === "BusinessLegalInformation" ? "active-business" : ""}`}
+    onClick={() => handleClick("BusinessLegalInformation")}
+  >
+    Business Legal Information
+  </button>
+  <button
+    className={`menu-bar-button ${activeComponent === "BankDetails" ? "active-bank-details" : ""}`}
+    onClick={() => handleClick("BankDetails")}
+  >
+    Bank Details
+  </button>
+  <button
+    className={`menu-bar-button ${activeComponent === "ContactPersons" ? "active-contact-persons" : ""}`}
+    onClick={() => handleClick("ContactPersons")}
+  >
+    Contact Persons
+  </button>
+</div>
       {activeComponent === "Addresses" && <AddressesComponent />}
       {activeComponent === "PersonalDetails" && <PersonalDetailsComponent />}
       {activeComponent === "BusinessLegalInformation" && (
@@ -410,13 +477,13 @@ function AddressFields() {
               <i className="fa fa-plus-circle" aria-hidden="true"></i>
             </div>
           </button>
-          
+
           {/* Render the component conditionally */}
           {selectedComponent === "Component1" && (
-          <div onClick={handleFormClick}>
-            <AddressTypeForm />
-          </div>
-        )}
+            <div onClick={handleFormClick}>
+              <AddressTypeForm />
+            </div>
+          )}
           <div className="cp-input-with-dropdown">
             <select className="address-border">
               <option value="" disabled selected hidden>
@@ -431,17 +498,17 @@ function AddressFields() {
             </button>
           </div>
           {/* add button */}
-          <button className="button-container"  onClick={() => toggleComponent("Component2")}>
+          <button className="button-container" onClick={() => toggleComponent("Component2")}>
             <div className="add-plus-container">
               <i className="fa fa-plus-circle" aria-hidden="true"></i>
             </div>
           </button>
           {/* Render the component conditionally */}
-        {selectedComponent === "Component2" && (
-          <div onClick={handleFormClick}>
-            <AddNewBranch />
-          </div>
-        )}
+          {selectedComponent === "Component2" && (
+            <div onClick={handleFormClick}>
+              <AddNewBranch />
+            </div>
+          )}
 
           <div className="cp-input-with-dropdown">
             <select className="address-border">
@@ -464,26 +531,26 @@ function AddressFields() {
           </button>
 
           {/* Render the component conditionally */}
-        {selectedComponent === "Component3" && (
-          <div onClick={handleFormClick}>
-            <AddUnitDivision />
-          </div>
-        )}
+          {selectedComponent === "Component3" && (
+            <div onClick={handleFormClick}>
+              <AddUnitDivision />
+            </div>
+          )}
 
         </div>
 
         {/* Second Row */}
         <div className="cp-row">
-          <input type="text" placeholder="Door No*" className="address-border"/>
-          <input type="text" placeholder="Building Name" className="address-border"/>
-          <input type="text" placeholder="Street Name*" className="address-border"/>
-          <input type="text" placeholder="Area/Taluk*" className="address-border"/>
+          <input type="text" placeholder="Door No*" className="address-border" />
+          <input type="text" placeholder="Building Name" className="address-border" />
+          <input type="text" placeholder="Street Name*" className="address-border" />
+          <input type="text" placeholder="Area/Taluk*" className="address-border" />
         </div>
 
         {/* Third Row */}
         <div className="cp-row">
           <input type="text" placeholder="City*" className="address-border" />
-            <div className="cp-input-with-icon">
+          <div className="cp-input-with-icon">
             <input type="text" placeholder="State*" className="search-border" />
             <i
               class="fa fa-search new-search-icon-inside-input"
@@ -491,13 +558,13 @@ function AddressFields() {
             ></i>
           </div>
           <div className="cp-input-with-icon">
-            <input type="text" placeholder="District*" className="address-border"/>
+            <input type="text" placeholder="District*" className="address-border" />
             <i
               class="fa fa-search new-search-icon-inside-input"
               aria-hidden="true"
             ></i>
           </div>
-          <input type="text" placeholder="PIN Code*" className="address-border"/>
+          <input type="text" placeholder="PIN Code*" className="address-border" />
         </div>
 
         {/* Fourth row */}
@@ -509,7 +576,7 @@ function AddressFields() {
               aria-hidden="true"
             ></i>
           </div>
-          <input type="text" placeholder="GST Number*" className="address-border"/>
+          <input type="text" placeholder="GST Number*" className="address-border" />
           <div className="cp-input-with-icon">
             <input type="text" placeholder="Place of Supply*" className="address-border" />
             <i
@@ -525,7 +592,7 @@ function AddressFields() {
 
 function PersonalDetailsComponent() {
 
-  
+
   return (
     <div>
       {/* Personal Details */}
@@ -634,13 +701,13 @@ function PersonalDetailFields() {
         {/* Fourth Row */}
         <div className="cp-row">
           <div className="cp-input-with-icon" >
-            <input type="text" placeholder="Nationality" className="custom-border" /> 
+            <input type="text" placeholder="Nationality" className="custom-border" />
             <i
-               class="fa fa-search new-search-icon-inside-input"
-              aria-hidden="true" 
+              class="fa fa-search new-search-icon-inside-input"
+              aria-hidden="true"
             ></i>
-           </div>
-        
+          </div>
+
           <div className="cp-input-with-dropdown">
             <select className="custom-border">
               <option
@@ -667,10 +734,10 @@ function PersonalDetailFields() {
           </button>
 
           {selectedComponent === "Component4" && (
-          <div onClick={handleFormClick}>
-            <AddNewDesignation />
-          </div>
-        )}
+            <div onClick={handleFormClick}>
+              <AddNewDesignation />
+            </div>
+          )}
 
         </div>
       </div>
@@ -908,10 +975,10 @@ function BankDetailFields() {
             </div>
           </button>
           {selectedComponent === "Component5" && (
-          <div onClick={handleFormClick}>
-            <AddAccountType />
-          </div>
-        )}
+            <div onClick={handleFormClick}>
+              <AddAccountType />
+            </div>
+          )}
 
         </div>
         {/* Third Row */}
@@ -947,7 +1014,7 @@ function BankDetailFields() {
 
 function ContactPersonsComponent() {
 
- 
+
   return (
     <div>
       {/* Contact Details */}
@@ -987,14 +1054,17 @@ function ContactPersonDetailFields() {
       <div className="cp-column">
         {/* First Row */}
         <div className="cp-row">
-          <div className="cp-input-with-icon mobile">
-            <span className="country-code">Mrs.</span>
-            <input
-              type="text"
-              placeholder="Person Name*"
-              className="customContact-border"
-            />
-          </div>
+        <div class="cp-input-with-icon mobile">
+          <div class="inputs-container">
+            <div class="mobileNumer-dropdown">
+              <select class="mobileDropDown"><option value="miss">Mr</option>
+              <option value="miss">Miss</option>
+              </select>
+              <span class="dropdown-icon">
+                </span></div></div>
+                <input type="text" placeholder="Person Name" class=""/>
+                </div>
+                <span className="separator"></span>
           <div className="cp-input-with-dropdown">
             <input
               type="text"
@@ -1010,40 +1080,48 @@ function ContactPersonDetailFields() {
           </button>
 
           {selectedComponent === "Component6" && (
-          <div onClick={handleFormClick}>
-            <AddNewDesignation />
-          </div>
-        )}
+            <div onClick={handleFormClick}>
+              <AddNewDesignation />
+            </div>
+          )}
 
         </div>
 
         {/* Second Row */}
         <div className="cp-row">
-          <div className="cp-input-with-icon mobile">
-            <span className="country-code">+91</span>
-            <input
-              type="text"
-              placeholder="Mobile Number*"
-              className="customContact-border"
-            />
-          </div>
+        <div class="cp-input-with-icon mobile">
+          <div class="inputs-container">
+            <div class="mobileNumer-dropdown">
+              <select class="mobileDropDown"><option value="miss">+91</option>
+              <option value="miss">Miss</option>
+                </select>
+              <span class="dropdown-icon">
+                </span></div></div>
+                <input type="text" placeholder="Mobile Number*"/>
+                </div>
+                <span className="separator"></span>
 
           <div className="cp-input-with-dropdown">
             <input
               type="text"
               placeholder="Email ID"
-              className="customContact-border"
-            />
+              className="customContact-border"/>
           </div>
 
-          <div className="cp-input-with-icon mobile">
-            <span className="country-code">0443</span>
-            <input
-              type="text"
-              placeholder="24816100"
-              className="customContact-border"
-            />
-          </div>
+          <div class="cp-input-with-icon mobile">
+          <div class="inputs-container">
+            <div class="mobileNumer-dropdown">
+              <select class="mobileDropDown"><option value="miss">0443</option>
+              <option value="miss">Miss</option>
+                </select>
+              <span class="dropdown-icon">
+                </span></div></div>
+                <input type="text" placeholder="24816100"/>
+                <span className="separator"></span>
+                </div>
+               
+
+
         </div>
 
         {/* Third Row */}
